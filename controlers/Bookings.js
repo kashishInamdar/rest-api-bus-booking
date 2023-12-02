3baf10b6b9c2d2d319665b9cf0a5203ebce6934a
kashishInamdar <kashishinamdar2004@gmail.com>
2023-12-02 21:46:51 +0530

    git push

--- controlers/Bookings.js
+++ controlers/Bookings.js
@@ -38,50 +38,6 @@ const GetApiV1Bookings = async (req,res)=>{
     })
 }
 
-const putApiBooking = async (req,res)=>{
-    const {id} = req.params;
 
-    const {bus, passangerName, mobileNumber, seatNumber, to, from} = req.body;
-
-    await Booking.updateOne({_id:id}, {$set:{bus, passangerName, mobileNumber, seatNumber, to, from}});
-
-    res.json({
-        success:true,
-        message:"successfully update booking"
-    })
-}
-
-const patchApiBooking = async (req,res)=>{
-    const {id} = req.params;
-
-    const { to, from} = req.body;
-
-    await Booking.updateOne({_id:id}, {$set: {to:to,from:from}})
-
-    res.json({
-        success:true,
-        message:"successfully update"
-    })
-
-}
-
-const deleteApiBooking = async (req,res)=>{
-   try{
-    const {id} = req.params;
-
-    const deleteBooking = await Booking.deleteOne({_id:id});
-    res.json({
-        success:true,
-        data:deleteBooking,
-        message:"successfully delete"
-    })
-   }
-   catch(err){
-    res.json({
-        success:false,
-        message:err.message
-    })
-   }
-}
 
 export {PostApiV1Bookings , GetApiV1Bookings }
